2e361c4d6d4cf1e9a24c8b5fd775d4a4334a8363
Shaurya Guru <shauryaguru@Shauryas-MacBook-Air-847.local>
2026-02-20 11:32:15 +0530

    [Shaurya] age is calculated

---
+++ Birthyear.java
@@ -0,0 +1,9 @@
+public class Birthyear{
+    public static void main(String[] args){
+        int currentyear=2024;
+        int birthyear=2000;
+        int age=currentyear-birthyear;
+        System.out.println("age is :"+age);
+
+    }
+}
